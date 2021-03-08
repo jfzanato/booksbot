0e979fa82e13b9f413ab649d566c6ba1aa81c3c9
João Fernando Zanato <jfzanato@hotmail.com>
2021-03-08 08:50:06 -0300

    Update settings.py

--- books/settings.py
+++ books/settings.py
@@ -7,3 +7,4 @@ NEWSPIDER_MODULE = 'books.spiders'
 
 ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
+USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
